2eb900b47dfa7f3005ce5cfe009796a9e9d095a5
Douglas Santos <98539760+gomesrd@users.noreply.github.com>
2024-01-28 22:48:03 -0300

    chore: adjust host default

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import { cleanEnv, str, num } from 'envalid';
 const env = cleanEnv(process.env, {
   NODE_ENV: str({ default: 'dev', choices: ['local', 'dev', 'prod', 'docker', 'production']}),
   PORT: num({ default: 3000 }),
-  HOST: str({ default: 'localhost' }),
+  HOST: str({ default: '0.0.0.0' }),
   JWT_SECRET_KEY: str({ default: 'secret' }),
   DATABASE_URL: str({ default: '' }),
 });
