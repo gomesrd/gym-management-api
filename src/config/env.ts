11db09e6c4ba3ca65d504103b48d851df9d49031
Douglas Santos <douglaspg@outlook.com>
2024-01-30 09:28:03 -0300

    adjust vercel.json

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,8 @@ import { cleanEnv, str, num } from 'envalid';
 
 const env = cleanEnv(process.env, {
   NODE_ENV: str({ default: 'dev', choices: ['local', 'dev', 'prod', 'docker', 'production']}),
-  PORT: num({ default: 3000 }),
-  HOST: str({ default: '0.0.0.0' }),
+  PORT: num({ default: 80 }),
+  HOST: str({ default: '127.0.0.1' }),
   JWT_SECRET_KEY: str({ default: 'secret' }),
   DATABASE_URL: str({ default: '' }),
 });
