1bd66aa08bd457965a6ea2bc8118abba94f43e7d
Douglas Santos <douglaspg@outlook.com>
2024-01-31 09:51:46 -0300

    chore: update node_env type

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 import { cleanEnv, str, num } from 'envalid';
 
 const env = cleanEnv(process.env, {
-  NODE_ENV: str({ default: 'dev', choices: ['local', 'dev', 'prod', 'docker', 'production']}),
+  NODE_ENV: str({ default: 'dev', choices: ['local', 'dev', 'prod', 'development', 'docker', 'production']}),
   PORT: num({ default: 80 }),
   HOST: str({ default: '127.0.0.1' }),
   JWT_SECRET_KEY: str({ default: 'secret' }),
