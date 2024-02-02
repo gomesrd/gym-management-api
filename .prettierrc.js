4b993c7d87c4659e9b547b5ee797eb7b171432c1
Douglas Santos <douglaspg@outlook.com>
2024-02-02 08:03:13 -0300

    chore: add pretier in project

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  insertPragma: false,
+  bracketSameLine: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+}
