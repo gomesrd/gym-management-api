ff3be050164f8a8edbd2bf018291318cfef7ff95
Douglas Santos <douglaspg@outlook.com>
2024-02-13 22:03:23 -0300

    update find many training replacement

--- src/modules/training/replacement/trainingReplacement.repository.ts
+++ src/modules/training/replacement/trainingReplacement.repository.ts
@@ -1,8 +1,8 @@
 import prisma from '../../../config/prisma'
 import { CreateTrainingReplacement } from '../training.schema'
 
-// import { Filters } from '../../../utils/common.schema'
-// import { FiltersPermissions } from '../../../utils/types'
+import { Filters } from '../../../utils/common.schema'
+import { FiltersPermissions } from '../../../utils/types'
 
 export async function createTrainingReplacement(input: CreateTrainingReplacement) {
   const { member_id } = input
@@ -25,67 +25,84 @@ export async function createTrainingReplacement(input: CreateTrainingReplacement
   })
 }
 
-//
-// export async function findManyTrainingsReplacement(filters: Filters, parseFilters: FiltersPermissions) {
-//   const countTrainingReplacement = await prisma.trainingReplacement.count({
-//     where: {
-//       realized: parseFilters.realized,
-//       member_id: filters.member_id
-//     }
-//   })
-//   const trainingReplacement = await prisma.trainingReplacement.findMany({
-//     where: {
-//       realized: parseFilters.deleted,
-//       member_id: filters.member_id
-//     },
-//     select: {
-//       id: true,
-//       realized: true,
-//       member: {
-//         include: {
-//           user: {
-//             select: {
-//               id: true,
-//               name: true
-//             }
-//           }
-//         }
-//       }
-//     }
-//   })
-//
-//   return {
-//     count: countTrainingReplacement,
-//     data: trainingReplacement
-//   }
-// }
-//
-// export async function findUniqueTrainingReplacement(trainingReplacementId: string, parseFilters: FiltersPermissions) {
-//   return prisma.trainingReplacement.findUnique({
-//     where: {
-//       id: trainingReplacementId,
-//       member_id: parseFilters.member_id,
-//       realized: parseFilters.realized
-//     },
-//     select: {
-//       id: true,
-//       realized: true,
-//       member: {
-//         include: {
-//           user: {
-//             select: {
-//               id: true,
-//               name: true
-//             }
-//           }
-//         }
-//       },
-//       created_at: true,
-//       updated_at: true
-//     }
-//   })
-// }
-//
+export async function findManyTrainingsReplacement(filters: Filters, parseFilters: FiltersPermissions) {
+  const countTrainingReplacement = await prisma.trainingReplacement.count({
+    where: {
+      realized: parseFilters.realized
+    }
+  })
+  const trainingReplacement = await prisma.trainingReplacement.findMany({
+    where: {
+      realized: parseFilters.deleted
+    },
+    select: {
+      id: true,
+      realized: true,
+      MemberReplacement: {
+        select: {
+          Member: {
+            select: {
+              user: {
+                select: {
+                  name: true
+                }
+              }
+            }
+          }
+        }
+      },
+      created_at: true,
+      updated_at: true
+    }
+  })
+
+  const trainingReplacementMap = trainingReplacement.map(training => {
+    return {
+      ...training,
+      members: training.MemberReplacement.map(member => member.Member.user.name).join('/')
+    }
+  })
+
+  return {
+    count: countTrainingReplacement,
+    data: trainingReplacementMap
+  }
+}
+
+export async function findUniqueTrainingReplacement(trainingReplacementId: string, parseFilters: FiltersPermissions) {
+  const findTrainingReplacement = await prisma.trainingReplacement.findUnique({
+    where: {
+      id: trainingReplacementId,
+      realized: parseFilters.realized
+    },
+    select: {
+      id: true,
+      MemberReplacement: {
+        select: {
+          Member: {
+            select: {
+              user: {
+                select: {
+                  id: true
+                }
+              }
+            }
+          }
+        }
+      },
+      created_at: true,
+      updated_at: true
+    }
+  })
+
+  return {
+    ...findTrainingReplacement,
+    members: findTrainingReplacement?.MemberReplacement.map(memberTraining => ({
+      id: memberTraining.Member.user.id
+    }))
+  }
+}
+
 export async function updateRealizedTrainingReplacement(trainingReplacementId: string) {
   return prisma.trainingReplacement.update({
     where: {
