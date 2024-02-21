98f9a4504d04f9388603a150a0710705c3ace852
Douglas Santos <douglas.santos@krykto.com>
2024-02-21 18:28:54 -0300

    change import module auth

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import { documentation } from './config/documentation'
-import { loginServer } from './modules/auth/login'
+import { validationAcessToken } from './modules/auth/validationAcessToken'
 import { authorizationServer } from './modules/auth/authorization'
 import { responseSchema } from './responseSchema'
 import { router } from './infra/router'
@@ -10,7 +10,7 @@ const setupServer = async (): Promise<{
   server: FastifyInstance
 }> => {
   await documentation(server)
-  await loginServer(server)
+  await validationAcessToken(server)
   await authorizationServer(server)
   await responseSchema(server)
   await router(server)
