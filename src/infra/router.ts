3c66f968e3dc0ee96c7c1be701ba9ee3664199a8
Douglas Santos <douglaspg@outlook.com>
2024-03-15 18:34:42 -0300

    add plans route

--- src/infra/router.ts
+++ src/infra/router.ts
@@ -1,13 +1,14 @@
 import { FastifyInstance } from 'fastify'
-import personalTrainerRoutes from '../modules/personalTrainer/personalTrainer.controller'
+import { routesPath } from '../utils/enumsController'
 import { healthCheck } from './health-check'
+import personalTrainerRoutes from '../modules/personalTrainer/personalTrainer.controller'
 import memberRoutes from '../modules/member/member.controller'
 import trainingRoutes from '../modules/training/training.controller'
 import trainingRecordRoutes from '../modules/trainingRecord/trainingRecord.controller'
 import trainingReportRoutes from '../modules/report/training/trainingReport.controller'
 import fastifyCors from '@fastify/cors'
-import { routesPath } from '../utils/enumsController'
 import authRoutes from '../modules/auth/login/login.controller'
+import plansRoutes from '../modules/plans/plans.controller'
 
 export async function router(server: FastifyInstance) {
   server.register(memberRoutes, { prefix: routesPath.members })
@@ -15,6 +16,7 @@ export async function router(server: FastifyInstance) {
   server.register(trainingReportRoutes, { prefix: routesPath.trainingsReport })
   server.register(trainingRecordRoutes, { prefix: routesPath.trainingsRecord })
   server.register(trainingRoutes, { prefix: routesPath.trainings })
+  server.register(plansRoutes, { prefix: routesPath.plans })
   server.register(authRoutes, { prefix: routesPath.login })
   server.register(healthCheck)
   server.register(fastifyCors, {
