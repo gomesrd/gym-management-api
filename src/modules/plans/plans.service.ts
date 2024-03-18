05a14f4141de9ec5667a8e7bf0a45ef372f2b251
Douglas Santos <douglaspg@outlook.com>
2024-03-18 07:25:42 -0300

    create plans service

---
+++ src/modules/plans/plans.service.ts
@@ -0,0 +1,101 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { createPlan, deletePlans, getManyPlans, getUniquePlan, updatePlans } from './plans.repository'
+import { PlanRegisterInput } from './plans.schema'
+import { Filters } from '../../utils/common.schema'
+
+export async function registerPlanHandler(
+  request: FastifyRequest<{
+    Body: PlanRegisterInput
+  }>,
+  reply: FastifyReply
+) {
+  const body = request.body
+
+  try {
+    await createPlan(body)
+
+    return reply.code(201).send('')
+  } catch (error: any) {
+    console.log(error)
+
+    return reply.code(500).send('Internal server error')
+  }
+}
+
+export async function getUniquePlanHandler(
+  request: FastifyRequest<{
+    Params: { plan_id: string }
+  }>,
+  reply: FastifyReply
+) {
+  const planId = request.params.plan_id
+  try {
+    const getPlan = await getUniquePlan(planId)
+
+    return reply.code(200).send(getPlan)
+  } catch (error: any) {
+    console.log(error)
+
+    return reply.code(500).send('Internal server error')
+  }
+}
+
+export async function getManyPlansHandler(
+  request: FastifyRequest<{
+    Querystring: Filters
+  }>,
+  reply: FastifyReply
+) {
+  try {
+    const filters = request.query
+    const getPlans = await getManyPlans(filters)
+
+    return reply.code(200).send(getPlans)
+  } catch (error: any) {
+    console.log(error)
+
+    return reply.code(500).send('Internal server error')
+  }
+}
+
+export async function updatePlansHandler(
+  request: FastifyRequest<{
+    Body: PlanRegisterInput
+    Params: { plan_id: string }
+  }>,
+  reply: FastifyReply
+) {
+  const planId = request.params.plan_id
+  const dataUpdate = request.body
+
+  try {
+    const update = await updatePlans(planId, dataUpdate)
+
+    return reply.code(200).send(update)
+  } catch (e: any) {
+    console.log(e)
+
+    return reply.code(500).send('Internal server error')
+  }
+}
+
+export async function deletePlansHandler(
+  request: FastifyRequest<{
+    Params: { plan_id: string }
+  }>,
+  reply: FastifyReply
+) {
+  const planId = request.params.plan_id
+
+  try {
+    await deletePlans(planId)
+
+    return reply.code(200).send('')
+  } catch (e: any) {
+    console.log(e)
+
+    return reply.code(500).send('Internal server error')
+  }
+}
+
+// TODO: Incluir tratamento para criação de plano que já existe
