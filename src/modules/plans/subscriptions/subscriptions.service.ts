4e8ab5ed3d9de70b5c96ada04f253152f7f6abca
Douglas Santos <douglaspg@outlook.com>
2024-03-19 22:54:08 -0300

    add service for subscriptions plans

---
+++ src/modules/plans/subscriptions/subscriptions.service.ts
@@ -0,0 +1,41 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { planSubscriptionBodyInput } from './subscriptions.schema'
+import { getUniquePlan } from '../plans/plans.repository'
+import { getDatesForMemberPlans } from '../../../utils/getFutereDate'
+import { createPlansSubscriptions } from './subscriptions.repository'
+
+export async function registerPlanSubscriptionHandler(
+  request: FastifyRequest<{
+    Body: planSubscriptionBodyInput
+  }>,
+  reply: FastifyReply
+) {
+  const { plan_id, member_id } = request.body
+
+  const planSelected = await getUniquePlan(plan_id)
+
+  if (!planSelected) {
+    return reply.code(404).send('Plan not found')
+  }
+
+  const { recurrence } = planSelected
+
+  const { starts_at, ends_at } = getDatesForMemberPlans(recurrence)
+
+  const data = {
+    plan_id,
+    member_id,
+    starts_at,
+    ends_at
+  }
+
+  try {
+    await createPlansSubscriptions(data)
+
+    return reply.code(201).send('')
+  } catch (error: any) {
+    console.log(error)
+
+    return reply.code(500).send('Internal server error')
+  }
+}
