2cbf3d67655efac781948ee05f6943ec85e5aa10
Douglas Santos <douglaspg@outlook.com>
2024-06-25 18:51:33 -0300

    create subscription findUnique repository

--- src/modules/plans/subscriptions/subscriptions.repository.ts
+++ src/modules/plans/subscriptions/subscriptions.repository.ts
@@ -1,5 +1,4 @@
 import prisma from '../../../config/prisma'
-import { Filters } from '../../../utils/common.schema'
 import { planSubscriptionDataInput } from './subscriptions.schema'
 
 export async function createPlansSubscriptions(data: planSubscriptionDataInput) {
@@ -15,4 +14,21 @@ export async function createPlansSubscriptions(data: planSubscriptionDataInput) 
   }
 
   return prisma.memberPlan.createMany(memberPlansData)
+}
+
+export async function getUniquePlanSubscriptionMember(member_plan_id: string) {
+  return prisma.memberPlan.findUnique({
+    where: {
+      id: member_plan_id
+    },
+    select: {
+      id: true,
+      plan_id: true,
+      plan: {
+        select: {
+          training_amount: true
+        }
+      }
+    }
+  })
 }
